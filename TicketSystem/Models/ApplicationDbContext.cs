fe864b5a1c27584c69328eed1aac8374031308c4
Sabir Halil <sabir47halil@gmail.com>
2024-06-02 01:46:05 +0300

    Add project files.

---
+++ TicketSystem/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TicketSystem.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+     
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<MilesSmilesAccount> MilesSmilesAccounts { get; set; }
+        public DbSet<Airport> Airports { get; set; }
+    }
+}
